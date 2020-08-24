083b6a423ccb98444e1f34d3bcb48857f9acef2b
debbie chong <giyoungchong1980@gmail.com>
2020-08-23 20:55:57 -0400

    add form

--- public/math.js
+++ public/math.js
@@ -1,1 +1,28 @@
 console.log('test output')
+
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function minus(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1/num2;
+}
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
